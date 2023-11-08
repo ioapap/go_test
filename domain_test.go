4ff713ad2b030ce9f0fbfc6a5c5347a4ccd8ecf2
Ioannis <powerfulguy4774@gmail.com>
2023-11-08 13:41:32 +0100

    Add tests for non-existent file, invalid CSV, and empty line handling

--- domain_test.go
+++ domain_test.go
@@ -3,8 +3,10 @@
 package main
 
 import (
+	"os"
 	"strings"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -21,3 +23,31 @@ Bonnie,Ortiz,bortiz1@cyberchimps.com,Female,197.54.209.129`
 	assert.Equal(t, want, got) // Assert that the 'got' slice should equal the 'want' slice.
 }
 
+func TestCountDomainsFileDoesNotExist(t *testing.T) {
+	_, err := CountDomains(os.Stdin)                                                                                 // assuming we're not actually reading from Stdin
+	assert.Error(t, err, "An error was expected when trying to count domains from a non-existent file, but got nil") // Assert that an error should have been returned.
+}
+
+func TestCountDomainsInvalidCSV(t *testing.T) {
+	const invalidCSVData = `first_name|last_name|email|gender|ip_address
+Mildred|Hernandez|mhernandez0@github.io|Female|38.194.51.128
+Bonnie|Ortiz|bortiz1@cyberchimps.com|Female|197.54.209.129` // The | instead of ,
+
+	reader := strings.NewReader(invalidCSVData)
+	_, err := CountDomains(reader)
+	assert.Error(t, err, "An error was expected when trying to count domains from an invalid CSV, but got nil")
+}
+
+func TestCountDomainsWithEmptyLine(t *testing.T) {
+	const csvDataWithEmptyLine = `first_name,last_name,email,gender,ip_address
+
+Mildred,Hernandez,mhernandez0@github.io,Female,38.194.51.128
+Bonnie,Ortiz,bortiz1@cyberchimps.com,Female,197.54.209.129`
+
+	reader := strings.NewReader(csvDataWithEmptyLine)
+	got, err := CountDomains(reader)
+	assert.NoError(t, err) // Assert no error should have been returned despite the empty line
+
+	want := []KeyValue{{"cyberchimps.com", 1}, {"github.io", 1}}
+	assert.Equal(t, want, got) // Assert that the 'got' slice should equal the 'want' slice.
+}
